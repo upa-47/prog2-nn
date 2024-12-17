6370ce21182ac866e5f597e7b85a94a05021f8c9
Keiichi Kobayashi <j3332fto@sit.ac.jp>
2024-12-17 16:27:47 +0900

    deviceの自動認識を追加

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ import torchvision.transforms.v2 as transforms
 
 import models
 
-
+# GPUがあれば、'cuda' なければ'cpu' というデバイス名を設定
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # データセットの前処理を定義
 ds_transform = transforms.Compose([
     transforms.ToImage(),
@@ -53,9 +54,9 @@ for image_batch, label_batch in dataloader_train:
 # モデルをインスタンス化する
 model = models.MyModel()
 
-acc_train = models.test_accuracy(model, dataloader_train)
+acc_train = models.test_accuracy(model, dataloader_train, device=device)
 print(f'train accuracy: {acc_train*100:.3f}%')
-acc_test = models.test_accuracy(model, dataloader_test)
+acc_test = models.test_accuracy(model, dataloader_test, device=device)
 print(f'test accuracy: {acc_test*100:.3f}%')
 
 # 損失関数（誤差関数・ロス関数）の選択
@@ -79,26 +80,26 @@ for k in range(n_epochs):
 
     # 1 epoch の学習
     time_start = time.time()
-    loss_train = models.train(model, dataloader_test, loss_fn, optimizer)
+    loss_train = models.train(model, dataloader_test, loss_fn, optimizer, device=device)
     time_end = time.time()
     loss_train_history.append(loss_train)
     print(f'train loss: {loss_train:.3f} ({time_end-time_start:.1f}s)', end=', ')
 
     time_end = time.time()
-    loss_test = models.test(model, dataloader_test, loss_fn)
+    loss_test = models.test(model, dataloader_test, loss_fn, device=device)
     time_end = time.time()
     loss_test_history.append(loss_test)
     print(f'test loss: {loss_test:.3f} ({time_end-time_start:.1f}s)', end=', ')
 
     
     time_end = time.time()
-    acc_train = models.test_accuracy(model, dataloader_train)
+    acc_train = models.test_accuracy(model, dataloader_train, device=device)
     time_end = time.time()
     acc_train_history.append(acc_train)
     print(f'train accuracy: {acc_train*100:.3f}% ({time_end-time_start:.1f}s)', end=', ')
 
     time_end = time.time()
-    acc_test = models.test_accuracy(model, dataloader_test)
+    acc_test = models.test_accuracy(model, dataloader_test, device=device)
     time_end = time.time()
     acc_test_history.append(acc_test)
     print(f'test accuracy: {acc_test*100:.3f}% ({time_end-time_start:.1f}s)')
